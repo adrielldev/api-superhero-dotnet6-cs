4b12117addf096edac3e2f0469a8433811ac5f0a
adriel <adrieldev@outlook.com>
2023-01-16 13:38:35 -0300

    feat-super-hero-model

---
+++ SuperHero.cs
@@ -0,0 +1,13 @@
+namespace api_superhero_dotnet6_cs
+{
+    public class SuperHero
+    {
+        public int Id {get;set;}
+        public string Name {get;set;} = string.Empty;
+        public string FirstName{get;set;} = string.Empty;
+
+        public string LastName{get;set;} = string.Empty;
+
+        public string Place {get;set;} = string.Empty;
+    }
+}
